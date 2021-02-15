573d00515474a0c6b1d9c4ef100f45e3918e4fcb
Elisey09 <77900796+Elisey09@users.noreply.github.com>
2021-02-15 19:14:28 +0200

    Add files via upload

---
+++ занятие 1/script.js
@@ -0,0 +1,11 @@
+alert('Hello');
+
+let name = prompt('What is your name, bro?');
+
+let question = '? Really?';
+
+alert(name + question);
+
+let greeting = 'Nice to meet you, ';
+
+alert(greeting + name);
