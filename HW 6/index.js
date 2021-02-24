7468d5849b136831142b114bf2d8fb2746adf22f
Elisey09 <77900796+Elisey09@users.noreply.github.com>
2021-02-24 17:42:46 +0200

    Add files via upload

---
+++ HW 6/index.js
@@ -0,0 +1,23 @@
+const inputEl = document.createElement("input");
+const firstEl = document.getElementsByClassName("ul")[0];
+const button = document.createElement("Button");
+const ulEl = document.getElementsByClassName("ul")[0];
+
+button.innerText = "Add ";
+button.addEventListener("click", Click);
+
+firstEl.append(inputEl);
+
+firstEl.prepend(button);
+ulEl.prepend(inputEl);
+
+button.addEventListener("click", Click);
+
+let i = 1;
+
+function Click() {
+  const liEl = document.createElement("li");
+  const userText = i++ + " " + inputEl.value;
+  liEl.innerText = userText;
+  ulEl.appendChild(liEl);
+}
