b361629934719b9df202e94fdd5b01b80fe29c76
Elisey09 <77900796+Elisey09@users.noreply.github.com>
2021-03-04 15:39:37 +0200

    Add files via upload

---
+++ HW 8/script.js
@@ -0,0 +1,16 @@
+
+const inputEl = document.querySelector('input');
+const typeEl = document.querySelector('.type');
+const figureEl = document.querySelector('.figure');
+
+inputEl.addEventListener('change', onColorChange);
+typeEl.addEventListener('change', onTypeChange);
+
+function onTypeChange() {
+    figureEl.classList.remove('circle', 'rectangle', 'squire');
+    figureEl.classList.add(typeEl.value);
+};
+
+function onColorChange() {
+    figureEl.style.background = inputEl.value;
+};
